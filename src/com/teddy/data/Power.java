da4ba7d2f05cde2d1b40e3700d746a8e40095ac8
Alex <ad1444@mybristol.ac.uk>
2012-12-03 12:44:44 +0000

    Added the 3 main screens plus button to navigate

--- src/com/teddy/data/Power.java
+++ src/com/teddy/data/Power.java
@@ -1,43 +1,45 @@
 package com.teddy.data;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-public class UsageScreen extends Activity {
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class Power extends Activity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+ 
+        setContentView(R.layout.power);
         
-        setContentView(R.layout.usagescreen);
+        TextView text = (TextView ) findViewById(R.id.powertext);
+        text.setText("Power Screen");
         
-        TextView text = (TextView ) findViewById(R.id.usagetext);
-        text.setText("Usage Screen");
-        
-        
-        Button usage =(Button) findViewById(R.id.usagebutton);
+        Button usage =(Button) findViewById(R.id.usagebutton1);
         usage.setText("Comp");
         
-        Button stats =(Button) findViewById(R.id.statsbutton);
+        Button stats =(Button) findViewById(R.id.statsbutton1);
         stats.setText("Stats");
         
-        Button power =(Button) findViewById(R.id.powerbutton);
+        Button power =(Button) findViewById(R.id.powerbutton1);
         power.setText("Power");
         
         usage.setOnClickListener(new Button.OnClickListener(){
         	public void onClick(View v){
 
+            	Intent i = new Intent(getApplicationContext(),  UsageScreen.class);
+            	startActivity(i);
         	}
         });
               
         power.setOnClickListener(new Button.OnClickListener(){
         public void onClick(View v){
 
-        	Intent i = new Intent(getApplicationContext(),  Power.class);
-        	startActivity(i);
     	}
     });
         
--- src/com/teddy/data/Stats.java
+++ src/com/teddy/data/Stats.java
@@ -1,8 +1,11 @@
 package com.teddy.data;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -12,8 +15,42 @@ public class Stats extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
  
-        TextView text = new TextView(this);
-        text.setText("This is the stats screen");
-        setContentView(text);
+        setContentView(R.layout.stats);
+        
+        TextView text = (TextView ) findViewById(R.id.statstext);
+        text.setText("Stats Screen");
+        
+        Button usage =(Button) findViewById(R.id.usagebutton2);
+        usage.setText("Comp");
+        
+        Button stats =(Button) findViewById(R.id.statsbutton2);
+        stats.setText("Stats");
+        
+        Button power =(Button) findViewById(R.id.powerbutton2);
+        power.setText("Power");
+        
+        usage.setOnClickListener(new Button.OnClickListener(){
+        	public void onClick(View v){
+
+            	Intent i = new Intent(getApplicationContext(),  UsageScreen.class);
+            	startActivity(i);
+        	}
+        });
+              
+        power.setOnClickListener(new Button.OnClickListener(){
+        public void onClick(View v){
+
+        	Intent i = new Intent(getApplicationContext(), Power.class);
+        	startActivity(i);
+    	}
+    });
+        
+        stats.setOnClickListener(new Button.OnClickListener(){
+        	public void onClick(View v){
+
+            	
+        	}
+        });
+              
 	}
 }
