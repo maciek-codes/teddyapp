d890411090a71aa7727a3cb9191a46cea299c593
Alex <ad1444@mybristol.ac.uk>
2013-02-04 16:51:45 +0000

    intruction screen changes

--- src/com/teddy/data/Instr.java
+++ src/com/teddy/data/Instr.java
@@ -14,10 +14,19 @@ import android.os.Bundle;
 
 public class Instr extends Activity {
 
+    int sessionId;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
  
+        String value="1";
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            value = extras.getString("id");
+            
+        }
+        
         setContentView(R.layout.instr);
         
 
@@ -28,7 +37,10 @@ public class Instr extends Activity {
         text.setText("Instructions");
         
         TextView content = (TextView ) findViewById(R.id.contenttext);
-        content.setText("Welcome to Teddy! \n\n1. Introduction\n\nThis app monitors usage of computers in different labs across University of Bristol.\n\n2. How to use\n (i) Available: This shows you the number of computers currently available for usage. Use the drop-down menus to select the building and room number. This is up-to-date over the last 15 minutes.\n\n (ii) Power Cost: This shows you the power costs for the last 15 minutes and the possible savings. Savings can be made because of the high number of idle computers, especially during off-peak time.\n\n (iii) Statistics: Statistics displays the power costs for different computer labs over a certain period of time. You can choose to view the data on a weekly, monthly or annual basis.\n");
+        if(value.equals("1")) content.setText("Welcome to Teddy! \n\n1. Introduction\n\nThis app monitors usage of computers in different labs across University of Bristol.\n\n");
+        else if(value.equals("2")) content.setText("2. How to use\n (i) Available: This shows you the number of computers currently available for usage. Use the drop-down menus to select the building and room number. This is up-to-date over the last 15 minutes.\n\n (ii) Power Cost: This shows you the power costs for the last 15 minutes and the possible savings. Savings can be made because of the high number of idle computers, especially during off-peak time.\n\n (iii) Statistics: Statistics displays the power costs for different computer labs over a certain period of time. You can choose to view the data on a weekly, monthly or annual basis.\n");
+                
+        
         content.setMovementMethod(new ScrollingMovementMethod());
 	
         Button home =(Button) findViewById(R.id.homebutton);
@@ -48,11 +60,14 @@ public class Instr extends Activity {
             	startActivity(i);
         	}
         });
-             
+        final String translation=value;
         next.setOnClickListener(new Button.OnClickListener(){
         	public void onClick(View v){
 
             	Intent i = new Intent(getApplicationContext(),  Instr.class);
+            	if(translation.equals("1")) i.putExtra("id", "2");
+            	if(translation.equals("2")) i.putExtra("id", "2");
+
             	finish();
             	startActivity(i);
         	}
@@ -62,6 +77,8 @@ public class Instr extends Activity {
         	public void onClick(View v){
 
             	Intent i = new Intent(getApplicationContext(),  Instr.class);
+            	if(translation.equals("1")) i.putExtra("id", "1");
+            	if(translation.equals("2")) i.putExtra("id", "1");
             	finish();
             	startActivity(i);
         	}
