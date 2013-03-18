d47be933712079d7c3b15432de009f2d08b87ade
CristianCernatescu <cc1319@my.bristol.ac.uk>
2013-03-18 17:27:44 +0000

    hardcoded pass with server pass and a bug

--- src/com/teddy/data/LoginActivity.java
+++ src/com/teddy/data/LoginActivity.java
@@ -1,24 +1,25 @@
- /*cd = new ConnectionDetector(getApplicationContext());
-        if (!cd.isConnectingToInternet()) {
-        	AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        	alertDialog.setTitle("No Internet");
-        	alertDialog.setMessage("Please check your internet connection");
-        	alertDialog.show();
-        }
-        else{*/
-
 package com.teddy.data;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
+
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -37,6 +38,7 @@ public class LoginActivity extends Activity {
             "foo@example.com:hello",
             "bar@example.com:world"
     };
+    //);
 
     /**
      * The default email to populate the email field with.
@@ -47,7 +49,7 @@ public class LoginActivity extends Activity {
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
-
+    ConnectionDetector cd;
     // Values for email and password at the time of the login attempt.
     private String mEmail;
     private String mPassword;
@@ -65,6 +67,9 @@ public class LoginActivity extends Activity {
 
         setContentView(R.layout.activity_login);
 
+        // Hide input keyboard
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        
         // Set up the login form.
         mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
         mEmailView = (EditText) findViewById(R.id.email);
@@ -111,88 +116,142 @@ public class LoginActivity extends Activity {
         if (mAuthTask != null) {
             return;
         }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        mEmail = mEmailView.getText().toString();
-        mPassword = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        String PASS = getString(R.string.password);
-        int ok=0;
-        if( mPassword.equals( PASS ) ) ok=1;
-        // Check for a valid password.
-        if (TextUtils.isEmpty(mPassword)) {
-            mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        } else if (mPassword.length() < 4) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
         
-        /////////////////////////////////////////////////////////////////////////////////////////password check
-        else if(ok==0)
-        {
-            mPasswordView.setError(getString(R.string.error_incorrect_password));
-            focusView = mPasswordView;
-            cancel = true;
+        cd = new ConnectionDetector(getApplicationContext());
+        if (!cd.isConnectingToInternet()) {
+        	AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        	alertDialog.setTitle("No Internet");
+        	alertDialog.setMessage("Please check your internet connection");
+        	alertDialog.show();
         }
+        else{
+	        // Reset errors.
+	        mEmailView.setError(null);
+	        mPasswordView.setError(null);
 
-        String email11 = getString(R.string.full);
-        String email12 = getString(R.string.min);
-        String email13 = getString(R.string.shorcut);
-        String email14 = getString(R.string.shorcut2);
-        String emaila1= "@";
-        String emaila2= "@";
-        String emaila3= "@";
-        String emaila4= "@";
-        String email1 = emaila1.concat(email11);
-        String email2 = emaila2.concat(email12);
-        String email3 = emaila3.concat(email13);
-        String email4 = emaila4.concat(email14);
-        int ok2=0,ok3=0;
-        if( mEmail.endsWith(email1) || mEmail.endsWith(email2) || mEmail.endsWith(email3) || mEmail.endsWith(email4)) ok2=1;
-        if( mEmail.equals("guest") || mEmail.equals("Guest") ) ok3=1;
-        
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(mEmail)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        }
-        /////////////////////////////////////////////////////////////////////////////////////////password check
-      else if (!mEmail.contains("@")&&ok3==0) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-        else if (ok2==0&&ok3==0) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-        
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-            showProgress(true);
-            mAuthTask = new UserLoginTask();
-            mAuthTask.execute((Void) null);
+	        // Store values at the time of the login attempt.
+	        mEmail = mEmailView.getText().toString();
+	        mPassword = mPasswordView.getText().toString();
+
+	        boolean cancel = false;
+	        View focusView = null;
+
+	        int ok=0;
+
+	        //check hardcoded password on server
+	        String pass="no";
+	        try{pass = (String)HTTPfunction("http://service-teddy2012.rhcloud.com/password");
+	        }catch(Exception e){}
+	        if( mPassword.equals( pass ) ) ok=1;
+
+
+	        //check hardcoded password in app
+	        //String PASS = getString(R.string.password);
+	        //if( mPassword.equals( PASS ) ) ok=1;
+
+
+			// Check for a valid password.
+	        if (TextUtils.isEmpty(mPassword)) {
+	            mPasswordView.setError(getString(R.string.error_field_required));
+	            focusView = mPasswordView;
+	            cancel = true;
+	        }
+	        //internet hardcoded
+	        else if (pass.equals("No internet connection")) {
+
+	            mPasswordView.setError(getString(R.string.error_no_internet));
+	            focusView = mPasswordView;
+	            cancel = true;}
+
+
+	        else if (mPassword.length() < 4) {
+	            mPasswordView.setError(getString(R.string.error_invalid_password));
+	            focusView = mPasswordView;
+	            cancel = true;
+	        }
+	        else if(ok==0)
+	        {
+	            mPasswordView.setError(getString(R.string.error_incorrect_password));
+	            focusView = mPasswordView;
+	            cancel = true;
+	        }
+
+	        String email11 = getString(R.string.full);
+	        String email12 = getString(R.string.min);
+	        String email13 = getString(R.string.shorcut);
+	        String email14 = getString(R.string.shorcut2);
+	        String emaila1= "@";
+	        String emaila2= "@";
+	        String emaila3= "@";
+	        String emaila4= "@";
+	        String email1 = emaila1.concat(email11);
+	        String email2 = emaila2.concat(email12);
+	        String email3 = emaila3.concat(email13);
+	        String email4 = emaila4.concat(email14);
+	        int ok2=0,ok3=0;
+	        if( mEmail.endsWith(email1) || mEmail.endsWith(email2) || mEmail.endsWith(email3) || mEmail.endsWith(email4)) ok2=1;
+	        if( mEmail.equals("guest") || mEmail.equals("Guest") ) ok3=1;
+
+	        // Check for a valid email address.
+	        if (TextUtils.isEmpty(mEmail)) {
+	            mEmailView.setError(getString(R.string.error_field_required));
+	            focusView = mEmailView;
+	            cancel = true;
+	        }
+	        /////////////////////////////////////////////////////////////////////////////////////////password check
+	       else if (!mEmail.contains("@")&&ok3==0) {
+
+	            mEmailView.setError(getString(R.string.error_invalid_email));
+	            focusView = mEmailView;
+	            cancel = true;
+	        }
+	        else if (ok2==0&&ok3==0) {
+	            mEmailView.setError(getString(R.string.error_invalid_email));
+	            focusView = mEmailView;
+	            cancel = true;
+
+	        }
+
+	       
+
+
+	        if (cancel) {
+	            // There was an error; don't attempt login and focus the first
+	            // form field with an error.
+	            focusView.requestFocus();
+	        } else {
+	            // Show a progress spinner, and kick off a background task to
+	            // perform the user login attempt.
+	            mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+	            showProgress(true);
+	            mAuthTask = new UserLoginTask();
+	            mAuthTask.execute((Void) null);
+	        }
         }
     }
 
+    
+
+    public String HTTPfunction(String getURL) {
+		try {
+		    HttpClient client = new DefaultHttpClient();  
+		    HttpGet get = new HttpGet(getURL);
+		    HttpResponse responseGet = client.execute(get);  
+		    HttpEntity resEntityGet = responseGet.getEntity();  
+		    String response="No internet connection";
+		    if (resEntityGet != null) {  
+		        // do something with the response
+		        response = EntityUtils.toString(resEntityGet);
+		        //Log.i("GET RESPONSE", response);
+		        return response;
+		    }
+		} catch (Exception e) {
+		    //e.printStackTrace();
+		    System.out.println(e.getMessage());
+		}
+	     return "No internet connection";
+	}
+    
     /**
      * Shows the progress UI and hides the login form.
      */
@@ -281,4 +340,5 @@ public class LoginActivity extends Activity {
             showProgress(false);
         }
     }
+    
 }
